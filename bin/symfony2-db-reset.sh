c6e5f54573f1bfc9ba739d996b980dde3abf7a2c
Géb <hgabka@webtown.hu>
2016-04-14 13:57:53 +0200

    javitas

--- bin/symfony2-db-reset.sh
+++ bin/symfony2-db-reset.sh
@@ -1,4 +1,4 @@
-app/console doctrine:database:drop
-app/console doctrine:database:create
-app/console doctrine:migrations:migrate
-app/console doctrine:fixtures:load
+php app/console doctrine:database:drop --force
+php app/console doctrine:database:create
+php app/console doctrine:migrations:migrate --no-interaction
+php app/console load:fixtures --no-interaction
